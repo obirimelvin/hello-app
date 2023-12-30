68e8ed1922cdb7071b4086d971ef9ba624e0dd1d
obirimelvin <obirimelvin@gmail.com>
2023-12-30 14:54:23 -0400

    modified hello to make server listen on 8080

--- hello.js
+++ hello.js
@@ -3,6 +3,19 @@
 // Import the 'fs' module
 const fs = require('fs');
 
-// Print "Hello, World!" to the console
-console.log('Hello, World!');
+//Import the 'express' module
+//
+
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req,res) => {
+ res.send('Hello, Word!');
+});
+
+app.listen(port, '0.0.0.0', () => {
+//Server Listening port
+ console.log(`Server listening at http://localhost:${port}`);
+});
 
